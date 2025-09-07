65561ce1b3705f1915dc5c7e951caa984199b766
Saad1926Q <saad.ahmed1926q@gmail.com>
2025-09-07 17:53:52 +0000

    did a lot of refactoring ; need to add append buffer and some more stuff

--- main.cpp
+++ main.cpp
@@ -1,75 +1,164 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include <ncurses.h>
 
 
-int main(int argc, char* argv[])
-{
-    std::string filename{argv[1]};
+struct textRow{
+	int size;
+	std::string text;
+};
+
+class AppendBuffer{
+	std::string buffer;
+};
 
 
-    std::ifstream file{filename};
+class EditorState{
+	int terminalRows;
+	int terminalCols;
+	int numRows;
+	std::vector<textRow> row;
+	
+
+	public:
+		EditorState(){
+			numRows=0;
+		}
+	
+		int getRows(){return terminalRows;}
+		int getCols(){return terminalCols;}
+
+		void setRows(int rows){terminalRows=rows;}
+		void setCols(int cols){terminalCols=cols;}
+
+
+		int getWindowSize(){
+			int rows,cols;
+			getmaxyx(stdscr,cols,rows);
+
+			if(rows==0||cols==0)
+				return -1;
+
+			setRows(cols);
+			setCols(rows);
+
+			return 0;
+		}
+
+		void appendRow(std::string text){
+			textRow txtRow;
+			txtRow.text=text;
+			txtRow.size=text.size();
+
+			numRows+=1;
+			row.push_back(txtRow);
+		}
+
+
+};
+
+EditorState E;
+
+
+void die(std::string errorMessage){
+	endwin();
+	std::cerr<<errorMessage<<std::endl;
+	std::exit(EXIT_FAILURE);
+}
+
+
+void initEditor(){
+	
+	if(E.getWindowSize()==-1)
+		die("getWindowSize");
+}
+
+
+void editorProcessKeypress() {
+	int ch=getch();
+
+	int r,c;
+
+	getyx(stdscr,r,c);
+
+	switch(ch){
+		case KEY_UP:
+			if(r!=0)
+				r-=1;
+			break;
+		case KEY_DOWN:
+			if(r!=LINES-1)
+				r+=1;
+			break;
+
+		case KEY_RIGHT:
+			if(c!=COLS-1)
+				c+=1;
+			break;
+		
+		case KEY_LEFT:
+			if(c!=0)
+				c-=1;
+			break;
+
+		case KEY_F(1):
+			endwin();
+			std::exit(0);
+			break;
+	}
+
+	move(r,c);
+
+}
+
+void handleFile(char* argv[]){
+	std::string filename{argv[1]};
+	
+	std::ifstream file{filename};
 
     if(!file){
         std::cerr<<"file couldnt be read!!\n";
-        return 1;
+        std::exit(EXIT_FAILURE);
     }
-	
-
-
-	initscr();
-	keypad(stdscr, TRUE);
-	clear(); 
-
 
 	std::string currentLine{};
 	int curr_row=0;
 
 	while(std::getline(file,currentLine)){
 		mvprintw(curr_row,0,"%s",currentLine.c_str());
+		E.appendRow(currentLine);
 
 		curr_row++;
 
 		if(curr_row>=LINES-1)
 			break;
 	}
+}
+
+
+int main(int argc, char* argv[])
+{	
+	initscr();
+	raw();
+	noecho();
+	keypad(stdscr, TRUE);
+	clear(); 
+
+	initEditor();
+
+	if(argc>=2){
+		handleFile(argv);
+	}else{
+		mvprintw(0,0,"Welcome to Sora Editor --version 1.0");
+	}
 
 	refresh();
 
-	int ch;
-
-	int r,c;
-
-	
-	while((ch = getch()) != KEY_F(1)){
-		getyx(stdscr,r,c);
-
-		switch(ch){
-			case KEY_UP:
-				if(r!=0)
-					r-=1;
-				break;
-			case KEY_DOWN:
-				if(r!=LINES-1)
-					r+=1;
-				break;
-
-			case KEY_RIGHT:
-				if(c!=COLS-1)
-					c+=1;
-				break;
-			
-			case KEY_LEFT:
-				if(c!=0)
-					c-=1;
-				break;
-		}
-
-		move(r,c);
+	while(1){
+		editorProcessKeypress();
 	}
-
-    endwin();
     
     return 0;
 
