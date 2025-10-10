9624f34a71f88302163f128cda03815cc0c10d62
Saad1926Q <saad.ahmed1926q@gmail.com>
2025-10-10 17:26:01 +0000

    feat: add file-level cursor and character insertion support

---
+++ README.md
@@ -0,0 +1,25 @@
+# yuki
+
+**yuki** is a simple terminal-based text editor written in C++.  
+The idea is to build a lightweight editor from scratch, focusing first on core editing features.
+
+## Goals
+
+The initial goal is to implement:
+- Basic insertion
+- Basic deletion
+- Basic file operations
+
+It's not aiming to be a full-fledged editor (yet), just something minimal that works.
+
+## Dependencies
+
+- [`ncurses`](https://invisible-island.net/ncurses/): used for handling terminal input and display.
+
+## Build
+
+To build the project, make sure you have `g++` and `ncurses` installed, then:
+
+```bash
+g++ main.cpp -lncurses -o yuki
+```
--- main.cpp
+++ main.cpp
@@ -36,12 +36,10 @@ class EditorState{
 	int colOffset;
 	int cursorX;
 	int cursorY;
+	int cursorFileY;  // This tells us which row in the file the cursor is on
+	int cursorFileX;  // This tells us which col in the file the cursor is on
 
-	
-
-
-	std::vector<textRow> row;
-	
+	std::vector<textRow> textRows;
 
 	public:
 		EditorState(){
@@ -50,6 +48,7 @@ class EditorState{
 			colOffset=0;
 			cursorX=0;
 			cursorY=0;
+			cursorFileY=0;
 		}
 	
 		int getRows(){return terminalRows;}
@@ -60,7 +59,10 @@ class EditorState{
 		int getCursorX(){return cursorX;}
 		int getCursorY(){return cursorY;}
 
-		std::vector<textRow> getTextRows(){return row;}
+		int getCursorFileY(){return cursorFileY;}
+		int getCursorFileX(){return cursorFileX;}
+
+		std::vector<textRow> getTextRows(){return textRows;}
 
 
 		void setRows(int rows){terminalRows=rows;}
@@ -69,6 +71,8 @@ class EditorState{
 		void setColOffset(int val){colOffset=val;}
 		void setCursorX(int val){cursorX=val;}
 		void setCursorY(int val){cursorY=val;}
+		void setCursorFileY(int val){cursorFileY=val;}
+		void setCursorFileX(int val){cursorFileX=val;}
 
 
 		int getWindowSize(){
@@ -79,8 +83,8 @@ class EditorState{
 			if(rows==0||cols==0)
 				return -1;
 
-			setRows(cols);
-			setCols(rows);
+			setRows(rows);
+			setCols(cols);
 
 			return 0;
 		}
@@ -91,15 +95,33 @@ class EditorState{
 			txtRow.size=text.size();
 
 			numRows+=1;
-			row.push_back(txtRow);
+			textRows.push_back(txtRow);
 		}
+
+		void editorInsertChar(char ch,int r,int c){
+			int numFileRows=getNumRows();
+
+			if(r>=0 && r<numFileRows){
+				textRow &row=textRows[r];
+
+				if(c>=0 && c<row.size){
+					row.text.insert(c,1,ch);
+				}else if(c==row.size){
+					row.text+=ch;
+				}
+
+				row.size++;
+
+			}
+
+		}
+		
 
 
 };
 
 AppendBuffer aBuf;
 EditorState E;
-
 
 
 void die(std::string errorMessage){
@@ -112,59 +134,6 @@ void die(std::string errorMessage){
 void initEditor(){
 	if(E.getWindowSize()==-1)
 		die("getWindowSize");
-}
-
-
-void editorProcessKeypress() {
-	int ch=getch();
-
-	int r,c;
-	int rowOffset=E.getRowOffset();
-	int colOffset=E.getColOffset();
-
-	int terminalRows=E.getRows();
-	int terminalCols=E.getCols();
-
-	getyx(stdscr,r,c);
-
-	switch(ch){
-		case KEY_UP:
-			if(r!=0)
-				r-=1;
-			else if(r==0 && rowOffset!=0)
-				E.setRowOffset(rowOffset-1);
-			break;
-
-
-		case KEY_DOWN:
-			if(r!=LINES-1)
-				r+=1;
-			else if(r==LINES-1 && rowOffset!=E.getNumRows()-1)
-				E.setRowOffset(rowOffset+1);
-			break;
-
-		case KEY_RIGHT:
-			if(c!=COLS-1)
-				c+=1;
-			else
-				E.setColOffset(colOffset+1);
-			break;
-		
-		case KEY_LEFT:
-			if(c!=0)
-				c-=1;
-			else if(c==0 and colOffset!=0)
-				E.setColOffset(colOffset-1);
-			break;
-
-		case KEY_F(1):
-			endwin();
-			std::exit(0);
-			break;
-	}
-
-	E.setCursorY(r);
-	E.setCursorX(c);
 }
 
 
@@ -248,6 +217,115 @@ void refreshScreen(){
 	curs_set(1);
 }
 
+void editorRowInsertChar(){
+
+}
+
+
+void editorProcessKeypress() {
+	int ch=getch();
+
+	int r,c;
+	int rowOffset=E.getRowOffset();
+	int colOffset=E.getColOffset();
+
+	int terminalRows=E.getRows();
+	int terminalCols=E.getCols();
+
+	getyx(stdscr,r,c);
+
+	int currCursorFileY=E.getCursorFileY(); // Currently which line of the file is the cursor on.
+	int currCursorFileX=E.getCursorFileX(); // Currently which col of the file is the cursor on.
+
+	int currRowSize = 0; 
+	std::vector<textRow> textRows;
+
+	if(E.getNumRows()>0){
+		textRows=E.getTextRows();
+		textRow currTextRow=textRows[currCursorFileY];
+
+		currRowSize=currTextRow.size;
+	}
+
+	switch(ch){
+		case KEY_UP:
+			if(currCursorFileY!=0){
+				if(r!=0)
+					r-=1;
+				else if(r==0 && rowOffset!=0)
+					E.setRowOffset(rowOffset-1);
+				
+				E.setCursorFileY(currCursorFileY-1); // Move one line up
+
+				textRow newRow=textRows[currCursorFileY-1];
+				if(currCursorFileX>newRow.size){
+					E.setCursorFileX(newRow.size); // If the length of new line is smaller than the previous line then snap to the end of the new line
+					c=E.getCursorFileX()-colOffset;
+				}
+			}
+			break;
+
+		case KEY_DOWN:
+			if(currCursorFileY!=E.getNumRows()-1){
+				if(r!=LINES-1)
+					r+=1;
+				else if(r==LINES-1 && rowOffset!=E.getNumRows()-1)
+					E.setRowOffset(rowOffset+1);
+
+				E.setCursorFileY(currCursorFileY+1); // Move one line down
+				
+				textRow newRow=textRows[currCursorFileY+1];
+				if(currCursorFileX>newRow.size){
+					E.setCursorFileX(newRow.size); // If the length of new line is smaller than the previous line then snap to the end of the new line
+					c=E.getCursorFileX()-colOffset;
+				}
+			}
+			
+			break;
+
+		case KEY_RIGHT:
+			if(currCursorFileX!=currRowSize){
+				if(c!=COLS-1)
+					c+=1;
+				else
+					E.setColOffset(colOffset+1);
+
+				E.setCursorFileX(currCursorFileX+1);
+			}
+			
+			break;
+		
+		case KEY_LEFT:
+			if(currCursorFileX!=0){
+				if(c!=0)
+					c-=1;
+				else if(c==0 and colOffset!=0)
+					E.setColOffset(colOffset-1);
+				
+				E.setCursorFileX(currCursorFileX-1);
+			}
+
+			break;
+
+		case KEY_F(1):
+			endwin();
+			std::exit(0);
+			break;
+
+		case KEY_ENTER:
+			break;
+
+		default:
+			if(isprint(ch)){
+				// Text insertion logic
+			}
+			break;
+	}
+
+	E.setCursorY(r);
+	E.setCursorX(c);
+}
+
 
 int main(int argc, char* argv[])
 {	
@@ -262,7 +340,7 @@ int main(int argc, char* argv[])
 	if(argc>=2){
 		handleFile(argv);
 	}else{
-		mvprintw(0,0,"Welcome to Sora Editor --version 1.0");
+		mvprintw(0,0,"Welcome to Yuki Editor --version 1.0");
 	}
 
 	refresh();
